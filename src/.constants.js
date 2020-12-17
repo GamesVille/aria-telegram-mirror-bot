0534d0789c232b7758b429643aaae79a796fd382
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2020-12-17 19:16:27 +0530

    Update and rename .constants.js.example to .constants.js

---
+++ src/.constants.js
@@ -0,0 +1,31 @@
+module.exports = Object.freeze({
+  TOKEN: '1171452793:AAFbS4MFGZKU1vy45_Iv21udAyMFYZ07d3o',
+  ARIA_SECRET: 'Myheroku',
+  ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir',
+  ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
+  ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
+  ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
+  GDRIVE_PARENT_DIR_ID: '0ACOSHvbjYDGEUk9PVA',
+  OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
+  SUDO_USERS: [922190664],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001494310839],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  STATUS_UPDATE_INTERVAL_MS: 5000, // A smaller number will update faster, but might cause rate limiting
+  DRIVE_FILE_PRIVATE: {
+    ENABLED: false,
+    EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
+  },
+  DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
+    enabled: false,   // Set this to true to use the notify functionality
+    host: 'hostname.domain',
+    port: 80,
+    path: '/botNotify'
+  },
+  COMMANDS_USE_BOT_NAME: {
+    ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
+    NAME: "@drivemir1_bot"
+  },
+  IS_TEAM_DRIVE: true,
+  USE_SERVICE_ACCOUNT: true,
+  INDEX_DOMAIN: "https://still-cake.clouddash.workers.dev/0:/" // add an extra / at the end, for example https://www.gdindex.url/
+});
